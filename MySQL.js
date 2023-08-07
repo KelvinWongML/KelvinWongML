ce9c8672aa4619523f012815e45f82fadc15c827
KelvinWongML <87269110+KelvinWongML@users.noreply.github.com>
2023-08-07 09:45:29 +0800

    Add files via upload

---
+++ MySQL.js
@@ -0,0 +1,20 @@
+let ws = new WebSocket('ws://localhost:3000')
+
+ws.onopen = () => {
+  console.log('open connection')
+}
+
+send = (username, password, type) => {
+  const message = [username, password, type]
+  ws.send(message)
+}
+
+ws.onclose = () => {
+  console.log('close connection')
+}
+
+ws.onmessage = event => {
+  document.getElementById('result').innerHTML = `${event.data.toString('utf8')}`
+}
+
+
