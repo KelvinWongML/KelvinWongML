52952fbd57ccd444bc299686efc15914728ed799
KelvinWongML <87269110+KelvinWongML@users.noreply.github.com>
2023-08-08 00:07:10 +0800

    server.js
    receives data from client side and compare them with data in MySQL database, then return response back to client side.

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const SocketServer = require('ws').Server
+const mysql = require('mysql')
+
+
+
+checkDB = (data, ws) => {
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "userinfo"
+  });
+
+  con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to MySQL database.");
+    switch (data[2]) {
+      case '0':
+        con.query(`SELECT EXISTS(SELECT * FROM user WHERE username = ? AND password = ?)`, [data[0], data[1]], function (err, result) {
+          con.end();
+          if (Object.values(result[0])[0] == 1) {
+            console.log('Client login.')
+            ws.send('login successfully.')
+          } else {
+            ws.send('login unsuccessfully.')
+          }
+          return;
+        });
+        break;
+
+      case '1':
+        con.query(`SELECT EXISTS(SELECT * FROM user WHERE username = ? )`, data[0], function (err, result) {
+          if (Object.values(result[0])[0] == 0) {
+            con.query(`INSERT INTO user VALUES(?, ?)`, [data[0], data[1]], (err, result) => {
+              con.end();
+              console.log('Client sign in.')
+              ws.send('Sign in successfully.')
+            })
+          } else {
+            con.end();
+            ws.send('Username is taken.')
+          }
+          return;
+        });
+        break;
+
+      default:
+        break;
+    };
+  });
+}
+
+
+const PORT = 3000
+
+const server = express()
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const wss = new SocketServer({ server })
+
+wss.on('connection', ws => {
+
+  console.log('Client connected')
+
+  ws.on('message', data => {
+    let message = data.toString('utf8').split(',')
+    checkDB(message, ws)
+  })
+
+  ws.on('close', () => {
+    console.log('Close connected')
+  })
+})
